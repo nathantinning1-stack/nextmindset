f955c6e06717547cb356036a28d43f118a1af286
nathantinning1-stack <nathantinning1@gmail.com>
2026-03-22 10:00:02 +0000

    fix page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main style={{ textAlign: "center", marginTop: "100px" }}>
+    <main style={{ textAlign: "center" , marginTop: "100px" }}>
       <h1>IT WORKS 🔥</h1>
     </main>
   );
