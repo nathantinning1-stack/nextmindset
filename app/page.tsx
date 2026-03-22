cda8548abc7e9d673acd2016e661ede5fb21e237
nathantinning1-stack <nathantinning1@gmail.com>
2026-03-22 10:04:56 +0000

    final fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main style={{ textAlign: "center" , marginTop: "100px" }}>
-      <h1>IT WORKS 🔥</h1>
-    </main>
+      <main style={{ textAlign: "center", marginTop: "100px" }}>
+        <h1>IT WORKS 🔥</h1>
+      </main>
   );
 }
